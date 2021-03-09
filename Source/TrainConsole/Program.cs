48d35013cbee965b69c939b12cf248676e1cb670
Randa <randamandawi@gmail.com>
2021-03-09 07:53:46 +0100

    Some classes

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -1,9 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TrainConsole
 {
     class Program
     {
+        public class Station
+        {
+            public int ID { get; set;}
+            public string Name { get; set;}
+
+
+        }
+
+        public class Train
+        {
+            public int ID { get; set;}
+            public string Name { get; set;}
+            public bool active { get; set;}
+
+            public void StartTrain()
+            {
+
+            }
+
+            public void StopTrain()
+            {
+
+            }
+
+        }
+
+        public class TimeSchedule
+        {
+            //public int TrainID;
+            public string StartStation;
+            public DateTime DepartureTime;
+            public string StopStation;
+            public DateTime ArrivalTime;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Train track!");
