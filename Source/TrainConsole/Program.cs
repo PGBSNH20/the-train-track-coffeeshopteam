4af8a7f2902404d0919a2591f9ea893f4bf0c268
Baverstrand <70134415+Baverstrand@users.noreply.github.com>
2021-03-09 09:46:42 +0100

    Sofie commit

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -6,43 +6,13 @@ namespace TrainConsole
 {
     class Program
     {
-        public class Station
-        {
-            public int ID { get; set;}
-            public string Name { get; set;}
-
-
-        }
-
-        public class Train
-        {
-            public int ID { get; set; }
-            public string Name { get; set; }
-            public int MaxSpeed { get; set; }
-            public bool Active { get; set; }
-
-            public void StartTrain()
-            {
-
-            }
-
-            public void StopTrain()
-            {
-
-            }
-        }
-
-        public class TimeSchedule
-        {
-            //public int TrainID;
-            public string StartStation;
-            public DateTime DepartureTime;
-            public string StopStation;
-            public DateTime ArrivalTime;
-        }
-
         static void Main(string[] args)
         {
+
+
+
+
+
             Console.WriteLine("Train track!");
             // Step 1:
             // Parse the traintrack (Data/traintrack.txt) using ORM (see suggested code)
--- Source/TrainEngine/ITrainPlanner.cs
+++ Source/TrainEngine/ITrainPlanner.cs
@@ -4,20 +4,113 @@ using System.Text;
 
 namespace TrainEngine
 {
+
     interface ITrainPlanner
     {
-        
-
-        // var travelPlan1 = new TrainPlaner(train1)
-
-        ITrainPlanner FollowSchedule(TrainSchedule schedule);
+        int TrainID { get; set; }
+        string TrainName { get; set; }
+        int MaxSpeed { get; set; }
+        int StationID { get; set; } // Till station class
+        string StationName { get; set; }
+        bool IsActive { get; set; }
+        ITrainPlanner StartTrain();
+        ITrainPlanner StopTrain();
         ITrainPlanner LevelCrossing();
-        // .CloseAt("10:23")
-        // .OpenAt("10:25")
-        // .SetSwitch(switch1, SwitchDirection.Left)
-        // .SetSwitch(switch2, SwitchDirection.Right)
-        // .ToPlan();
-       
+        ITrainPlanner RailSwitch();
+        ITrainPlanner ToPlan();
+    }
+    public class TrainPlanner : ITrainPlanner
+    {
+        public TrainPlanner(int trainID, string trainName, int maxSpeed, int stationID, string stationName, bool isActive)
+        {
+            TrainID = trainID;
+            TrainName = trainName;
+            MaxSpeed = maxSpeed;
+            StationID = stationID;
+            StationName = stationName;
+            IsActive = isActive;
+        }
+
+        public int TrainID { get; set; }
+        public string TrainName { get; set; }
+        public int MaxSpeed { get; set; }
+        public int StationID { get; set; }
+        public string StationName { get; set; }
+        public bool IsActive { get; set; }
+
+        public ITrainPlanner LevelCrossing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrainPlanner RailSwitch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrainPlanner StartTrain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrainPlanner StopTrain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrainPlanner ToPlan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+    // var travelPlan1 = new TrainPlaner(train1)
+
+    //ITrainPlanner FollowSchedule(TrainSchedule schedule);
+    //ITrainPlanner LevelCrossing();
+    // .CloseAt("10:23")
+    // .OpenAt("10:25")
+    // .SetSwitch(switch1, SwitchDirection.Left)
+    // .SetSwitch(switch2, SwitchDirection.Right)
+    // .ToPlan();
+
+    /*public class Station
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
 
     }
+
+    public class Train
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxSpeed { get; set; }
+        public bool Active { get; set; }
+
+        public void StartTrain()
+        {
+
+        }
+
+        public void StopTrain()
+        {
+
+        }
+    }*/
+
+    public class TimeSchedule
+        {
+            //public int TrainID;
+            public string StartStation;
+            public DateTime DepartureTime;
+            public string StopStation;
+            public DateTime ArrivalTime;
+        }
+        
+
 }
