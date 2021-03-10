8a873f7d91d26cabf2fd8f6fd493a00039d1d905
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-10 13:08:10 +0100

    Update ITrainPlanner.cs

--- Source/TrainEngine/ITrainPlanner.cs
+++ Source/TrainEngine/ITrainPlanner.cs
@@ -20,19 +20,47 @@ namespace TrainEngine
     }
 
     public class Train
-    {
+    { 
         public int ID { get; set; }
         public string Name { get; set; }
         public int MaxSpeed { get; set; }
         public bool IsActive { get; set; }
     }
 
-    public class TimeTable
+    //TraindId,StationId,DepartureTime,ArrivalTime
+    //2,1,10:20,null
+    //2,2,10:45,10:43
+    //2,3,null,10:59
+    //3,3,10:23,null
+    //3,4,10:55,10:53
+    //3,1,null,11:15
+
+    public class TimeTableEvent
     {
         public int TrainID { get; set; }
         public int StationID { get; set; }
         public DateTime DepartureTime { get; set; }
         public DateTime ArrivalTime { get; set; }
+        public bool IsDeparture { get; set; }
+        public bool IsFinalDestination { get; set; }
+    }
+
+    //public class TrialAndError
+    //{
+    //    bool IsDeparture;
+    //    public TrialAndError()
+    //    {
+    //        TimeTableEvent event1 = new TimeTableEvent { TrainID = 2, StationID = 1, DepartureTime = new DateTime(2021, 3, 10, 10, 20, 0) };
+    //        if (event1.ArrivalTime == null)
+    //        {
+    //            IsDeparture = true;
+    //        }
+    //    }
+    //}
+
+    public class TimeTable
+    {
+        public List<TimeTableEvent> Events { get; set; }
     }
 
     // maybe use enum for the switch direction.
