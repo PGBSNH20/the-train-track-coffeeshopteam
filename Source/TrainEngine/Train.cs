6e98ac22a8044f5b4d6b1d63f6b9818505d6f4c7
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-10 21:22:30 +0100

    Create Train.cs

---
+++ Source/TrainEngine/Train.cs
@@ -0,0 +1,14 @@
+﻿namespace TrainEngine
+{
+    public class Train
+    { 
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxSpeed { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
+
+        
+
+
