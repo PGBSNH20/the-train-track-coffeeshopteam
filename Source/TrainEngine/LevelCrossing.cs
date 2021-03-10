2e5d9e6435aa5e2c7c12ebf87a5958c170918986
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-10 21:22:47 +0100

    Create LevelCrossing.cs

---
+++ Source/TrainEngine/LevelCrossing.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainEngine
+{
+    // Maybe its needed maybe not? if not, just make the boolean a global variable.
+    public class LevelCrossing
+    {
+        // A bool to say if its open or closed
+        public bool IsOpen { get; set; }
+    }
+}
+
+        
+
+
