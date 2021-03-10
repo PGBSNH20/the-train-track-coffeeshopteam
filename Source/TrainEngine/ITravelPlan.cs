6621e612ca564346ad5974587003ff806c837ca5
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-10 18:08:54 +0100

    Create ITravelPlan.cs

---
+++ Source/TrainEngine/ITravelPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    public interface ITravelPlan
+    {
+        string StartStation { get; set; }
+        string StartTime { get; set; }
+        string ArriveStation { get; set; }
+        string ArriveTime { get; set; }
+
+        ITravelPlan StartAt(string station, string time);
+        ITravelPlan ArriveAt(string station, string time);
+        ITravelPlan GeneratePlan();
+    }
+}
