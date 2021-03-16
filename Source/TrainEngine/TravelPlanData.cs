cb032d8daad388cd689bbe175f3949444682966c
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-16 12:45:53 +0100

    Using Distance

--- Source/TrainEngine/TravelPlanData.cs
+++ Source/TrainEngine/TravelPlanData.cs
@@ -11,6 +11,7 @@ namespace TrainEngine
         public int ArriveStationID { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan ArriveTime { get; set; }
+        public int TimeDeviationInMinutes { get; set; }
         public bool HasStarted { get; set; }
         public bool HasArrived { get; set; }
     }
