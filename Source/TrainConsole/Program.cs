1b30ee8a803e5c1ba1e39a7fac288a74eea18bab
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-16 10:51:04 +0100

    Update Program.cs

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -11,11 +11,6 @@ namespace TrainConsole
         static void Main(string[] args)
         {
             Clock clock1 = new Clock();
-            //Thread.Sleep(100);
-            //for (int i = 0; i < 15; i++)
-            //{
-            //    clock1.ShowTime();
-            //}
 
             TrackOrm trackOrm = new TrackOrm();
             string track1Path = "Data/traintrack1.txt";
@@ -24,12 +19,12 @@ namespace TrainConsole
             List<string> track2Path = FileIO.GetDataFromFile("Data/traintrack2.txt");
             TrackDescription track2 = trackOrm.ParseTrackDescription(track2Path);
 
-            string trainsPath = "Data/trains.txt";
-            List<Train> trains = FileIO.ReadTrainInfo(trainsPath);
+            //string trainsPath = "Data/trains.txt";
+            //List<Train> trains = FileIO.ReadTrainInfo(trainsPath);
 
             ITravelPlan travelPlanSofie = new TravelPlanner().AddTrack(track2).SelectTrain(2).StartAt(1, "10:00").ArriveAt(2, "12:12").GeneratePlan();
             ITravelPlan travelPlanAle = new TravelPlanner().AddTrack(track2).SelectTrain(2).StartAt(1, "10:00").ArriveAt(2, "12:12").StartAt(2, "12:17").ArriveAt(3, "14:53").SelectTrain(3).StartAt(1, "12:30").ArriveAt(3, "17:00").GeneratePlan();
-            //ITravelPlan travelPlanShouldCrash = new TravelPlanner().AddTrack(track2).SelectTrain(2).StartAt(1, "10:00").ArriveAt(2, "12:12").SelectTrain(3).StartAt(2, "11:30").ArriveAt(1, "17:00").GeneratePlan();
+           // ITravelPlan travelPlanShouldCrash = new TravelPlanner().AddTrack(track2).SelectTrain(2).StartAt(1, "10:00").ArriveAt(2, "12:12").SelectTrain(3).StartAt(2, "11:30").ArriveAt(1, "17:00").GeneratePlan();
 
             //ITravelPlan travelplan2 = new TravelPlanner().AddTrain(trains[0]).StartAt(1, "10:30").ArriveAt(2, "12:30").GeneratePlan();
             //travelplan2.Simulate(clock1);
